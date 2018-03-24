630f895608fee2a8440b3392c02cf75dfd8b79e5
Artur Belogur <artur.belogur.korbit@gmail.com>
2018-03-24 14:14:28 +0200

    Fix arrows

--- js/gallery.js
+++ js/gallery.js
@@ -2,13 +2,12 @@ const $ = require("jquery");
 require("slick-carousel");
 
 $('#slider').slick({
-    lazyLoad: 'progressive',
+    lazyLoad: 'ondemand',
     speed: 500,
     fade: true,
     cssEase: 'linear',
     adaptiveHeight: true,
     prevArrow: '<div class="left-arrow"></div>',
     nextArrow: '<div class="right-arrow"></div>',
-    lazyload: true,
     arrowKeys: true
 })
